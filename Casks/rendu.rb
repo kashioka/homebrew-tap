3178a926e13438baeb0566a2d65df22e47859704
kashioka <kashioka@alleyoop.jp>
2026-04-09 17:54:11 +0900

    Update Rendu cask to v0.3.0
    - Switch from tar.gz to dmg
    - Add verified sha256

--- Casks/rendu.rb
+++ Casks/rendu.rb
@@ -1,8 +1,8 @@
 cask "rendu" do
-  version "0.2.0"
-  sha256 :no_check # TODO: リリース後に実際の sha256 に更新
+  version "0.3.0"
+  sha256 "7417c21653de72d992844b9a21d47d93cf68c5cd80af1cfe9e844b1162ca71e6"
 
-  url "https://github.com/kashioka/Rendu/releases/download/v#{version}/Rendu-macos-aarch64.tar.gz"
+  url "https://github.com/kashioka/Rendu/releases/download/v#{version}/Rendu_#{version}_aarch64.dmg"
   name "Rendu"
   desc "Lightweight desktop Markdown viewer"
   homepage "https://github.com/kashioka/Rendu"
